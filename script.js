5c8fef92d9e946773d3f8837175811208939770a
Umesh Kumar Meghwal <umeshmeghwa605@gmail.com>
2025-02-18 17:24:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ function closePopup() {
 function buy(){
     window.location.href = "information1.html";
 }
+
+
+       document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let formData = {
+        firstname: document.querySelector('input[name="firstname"]').value,
+        lastname: document.querySelector('input[name="lastname"]').value,
+        address: document.querySelector('input[name="address"]').value,
+        city: document.querySelector('input[name="city"]').value,
+        state: document.querySelector('input[name="state"]').value,
+        pincode: document.querySelector('input[name="pincode"]').value,
+        phone: document.querySelector('input[name="phone"]').value
+    };
+    // Form validation
+  
+        fetch("https://script.google.com/macros/s/AKfycbws8zo85G9mtvLCKzKPXRD_Nbd1JHcqDcRIuZyHs40QG0IOuYJxu2L4PFZO7hCqRAhg/exec", {  
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        mode: "no-cors",  
+        body: JSON.stringify(formData)
+        
+    })
+    document.getElementById("contactForm").reset();
+    alert("Your order has been placed successfully");
+    window.location.href = "https://pay.upilink.in/pay/80005096354-1@ybl?am=156";
+
+    });
+    
+      
+
